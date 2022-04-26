66074b50a0e37d2f4e5f9c3acaa886bb0086f665
Andrii <91153388+WeAreTheSameBlood@users.noreply.github.com>
2022-04-26 23:14:57 +0300

    @Cont

--- src/main/java/hlybchenko/controllers/FirstController.java
+++ src/main/java/hlybchenko/controllers/FirstController.java
@@ -2,11 +2,15 @@ package hlybchenko.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname){
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")
