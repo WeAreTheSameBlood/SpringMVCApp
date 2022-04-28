84eae985b742075968cd0baae14f77093eaf628e
Andrii <hlybchenko.andrii@gmail.com>
2022-04-28 20:58:55 +0300

    DAO

---
+++ src/main/java/hlybchenko/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package hlybchenko.dao;
+
+import hlybchenko.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Andrii"));
+        people.add(new Person(++PEOPLE_COUNT, "Serhii"));
+        people.add(new Person(++PEOPLE_COUNT, "Liza"));
+        people.add(new Person(++PEOPLE_COUNT, "Sasha"));
+        people.add(new Person(++PEOPLE_COUNT, "Dima"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,6 @@
 <html>
 <body>
 <h2>Hello World!</h2>
-<a href="/hello">to /hello</a>
-or <a href="/calculator">to Calc</a>
+<a href="/people">to /people</a>
 </body>
 </html>
