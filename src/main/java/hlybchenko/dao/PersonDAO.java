1b6923ee1a61d0142aa4c13c297e0eeafe0b988e
Andrii <hlybchenko.andrii@gmail.com>
2022-08-10 02:12:04 +0300

    SQL constraints (NOT NULL, UNIQUE, PRIMARY KEY, CHECK)

--- src/main/java/hlybchenko/dao/PersonDAO.java
+++ src/main/java/hlybchenko/dao/PersonDAO.java
@@ -3,6 +3,7 @@ package hlybchenko.dao;
 import hlybchenko.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
@@ -22,16 +23,16 @@ public class PersonDAO {
     }
 
     public List<Person> index(){
-        return jdbcTemplate.query("SELECT * FROM Person", new PersonMapper());
+        return jdbcTemplate.query("SELECT * FROM Person", new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id){
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[] {id}, new PersonMapper())
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[] {id}, new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES (1, ?, ?, ?)",
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES (?, ?, ?)",
                 person.getName(), person.getAge(), person.getEmail());
     }
 
