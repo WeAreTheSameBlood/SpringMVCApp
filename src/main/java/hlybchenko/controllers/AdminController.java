981258f5c809fb3fa20b0cfc4971f382d0f22905
Andrii <hlybchenko.andrii@gmail.com>
2022-08-11 13:13:53 +0300

    Added AdminController and adminPage.html with new tags: <form/>, <select/>,<options/>

---
+++ src/main/java/hlybchenko/controllers/AdminController.java
@@ -0,0 +1,32 @@
+package hlybchenko.controllers;
+
+import hlybchenko.dao.PersonDAO;
+import hlybchenko.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String adminPage(Model model, @ModelAttribute("person") Person person){
+        model.addAttribute("people", personDAO.index());
+        return "adminPage";
+    }
+
+    @PatchMapping("/add")
+    public String makeAdmin(@ModelAttribute("person") Person person) {
+        System.out.println(person.getId());
+        return "redirect:/people";
+    }
+}
